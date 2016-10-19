a8da9b83edf90c462c316d4ccfd1433e90065328
Larry <pudding850806@gmail.com>
2016-10-19 23:57:31 +0800

    add timeout option

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ var userInfo = {
     password: 'Your_Password'
 };
 
+var timeout = undefined; // 1000 for one second
+
+var inTimeout = {};
+
 login({email: userInfo.email, password: userInfo.password}, function(err, api){
     if(err) return console.log(err);
 
@@ -27,9 +31,19 @@ login({email: userInfo.email, password: userInfo.password}, function(err, api){
             return;
         }
 
+        console.log(message);
+
         var req = message.body ? message.body.toLowerCase() : ''; 
         var id = message.threadID;
-        if(req) handleMessage(req, id, sendMessage);
+        if(req && !inTimeout[id]){
+            handleMessage(req, id, sendMessage);
+            if(timeout){
+                inTimeout[id] = true;
+                setTimeout(function(){
+                    inTimeout[id] = false;
+                }, timeout);
+            }
+        }
     });
 
 });
